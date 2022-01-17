e28b4075a4f0a82265c6743a7c1972df5d98ecf6
Micaela Oertel <micaela.oertel@obspm.fr>
2022-01-17 13:54:33 +0100

    GPL added

--- compose.f90
+++ compose.f90
@@ -1,3 +1,25 @@
+!
+!   Copyright (c) 2013-2022 Stefan Typel, Marco Mancini, Micaela Oertel
+!
+!   This file is part of CompOSE
+ !
+ !   CompOSE is free software; you can redistribute it and/or modify
+ !   it under the terms of the GNU General Public License as published by
+ !   the Free Software Foundation; either version 2 of the License, or
+ !   (at your option) any later version.
+ !
+ !   CompOSE is distributed in the hope that it will be useful,
+ !   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ !   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ !   GNU General Public License for more details.
+ !
+ !   You should have received a copy of the GNU General Public License
+ !   along with CompOSE; if not, write to the Free Software
+ !   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ !
+ !
+
+
 !***********************************************************************
 !***********************************************************************
 ! Fortran90 program from the CompOSE website http://compose.obspm.fr
